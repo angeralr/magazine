d88c5a98588b1622536919c6035300d81b437f58
angeralr <56032043+angeralr@users.noreply.github.com>
2020-12-22 21:28:08 +0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/angeralr/magazine/"
+	// "github.com/headfirstgo/magazine"
+)
+
+func main() {
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.Street = "123 Oak St"
+	subscriber.City = "Omaha"
+	subscriber.State = "NE"
+	subscriber.PostalCode = "68111"
+	fmt.Println("Subscriber Name:", subscriber.Name)
+	fmt.Println("Street:", subscriber.Street)
+	fmt.Println("City:", subscriber.City)
+	fmt.Println("State:", subscriber.State)
+	fmt.Println("Postal Code:", subscriber.PostalCode)
+
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Employee Name:", employee.Name)
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
+}
